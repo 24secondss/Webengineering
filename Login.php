15636ab342bd60ee0e2521f61861e1232b4e0399
24secondss <78076663+24secondss@users.noreply.github.com>
2021-06-22 13:54:12 +0200

    Anmeldung verbessert
    Anmeldedaten liegen jetzt in einer Textdatei und die Anmeldung funktioniert darüber.

--- Login.php
+++ Login.php
@@ -10,28 +10,36 @@
 			$benutzername_input = $_POST["Benutzername_login"];
 			$passwort_input = $_POST["Passwort_login"];
 
-			if ($benutzername_input == 'Admin') {
-				if ($passwort_input == '1234') {
-					$ausgabe = "<p> Admin erfolgreich eingeloggt </p>";
-					$link = "<a href=\"Account.html\">Zurück zur Anmeldung </a>";
-					echo $ausgabe .= $link;
-				}
-			}
+			$file = "Anmeldedaten.txt";
+			$fhandle = fopen($file, "r");
 
-			elseif ($benutzername_input == 'TestUser') {
-				if ($passwort_input == 'test') {
-					$ausgabe = "<p> TestUser erfolgreich eingeloggt </p>";
-					$link = "<a href=\"Account.html\">Zurück zur Anmeldung </a>";
-					echo $ausgabe .= $link;
+			$i = 0;
+			$j = 0;
+			while ($line = fgets($fhandle)) {
+				$logindaten[$i] = $line;
+				if ($benutzername_input == trim($logindaten[$i])) {
+					$logindaten[$i + 1] = fgets($fhandle);
+					if ($passwort_input == trim($logindaten[$i + 1])) {
+						$ausgabe = "<p> Erfolgreich eingeloggt </p>";
+						$link = "<a href=\"Account.html\">Zurück zur Anmeldung </a>";
+						echo $ausgabe .= $link;	
+						$j++;
+					}
+					else {
+						$ausgabe = "<p> Passwort ungültig </p>";
+						$link = "<a href=\"Account.html\">Zurück zur Anmeldung </a>";
+						echo $ausgabe .= $link;	
+					}
 				}
+				$i++;
 			}
-
-			else {
-				$ausgabe = "<p> Benutzername oder Passwort inkorrekt </p>";
+			
+			if ($j == 0) {
+				$ausgabe = "<p> Benutzername ungültig </p>";
 				$link = "<a href=\"Account.html\">Zurück zur Anmeldung </a>";
 				echo $ausgabe .= $link;
 			}
 		?>
-		
+
 	</body>
 </html>
