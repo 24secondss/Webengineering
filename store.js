5fd10d3a9aa3bf3bae509874bcb0c36e3302cb62
MarinaMiriam <77719371+MarinaMiriam@users.noreply.github.com>
2021-07-25 19:48:15 +0200

    Bestellseite

--- store.js
+++ store.js
@@ -1,3 +1,5 @@
+//Warenkorb
+
 if (document.readyState == 'load') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -101,3 +103,79 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '€' + total
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+  // holt die ganzen HTML Dokumente ein
+
+  var form = document.getElementById("A New Form");
+  
+
+   form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data);
+  });
+});
+
+
+
+
+// Bestätigungsmail
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("A New Form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks!";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
+  
+    
+ 
+  
+
+
+
+
+ 
+  
+   
